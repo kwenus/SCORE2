32d985f70ccf47b0f17322343ccd0aed4dc8d786
kwenus <kwenus82@gmail.com>
2022-02-08 22:19:09 +0000

    2 com

--- SCORE2/SCORE2/Class Patient.cs
+++ SCORE2/SCORE2/Class Patient.cs
@@ -1,7 +1,20 @@
 ﻿class Patient
 {
-    public string FirstName;
-    public string LastName;
+    public string? FirstName;
+    public string? LastName;
+    public string? Sex;
     public int Age;
+    public Patient()
+    {
+        Console.WriteLine("Answer next questions.");
+        Console.WriteLine("What is your name?");
+        FirstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        LastName = Console.ReadLine();
+        Console.WriteLine("What is your sex?");
+        Sex = Console.ReadLine();
+        Console.WriteLine("What is your age");
+        Age = Convert.ToInt16(Console.ReadLine());
+    }
     public void PrintAll() => Console.WriteLine($"Patient: {FirstName}, {LastName}");
 }
--- SCORE2/SCORE2/Program.cs
+++ SCORE2/SCORE2/Program.cs
@@ -1,7 +1,3 @@
 ﻿Patient Kanatev = new Patient();
-Kanatev.FirstName = "Konstantin";
-Kanatev.LastName = "Kanatev";
-Kanatev.Age = 39;
 Kanatev.PrintAll();
-
 
