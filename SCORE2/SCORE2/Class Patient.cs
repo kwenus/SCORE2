599be0485ee733d667eed9eb21bc6cf1369a3754
kwenus <kwenus82@gmail.com>
2022-02-08 22:05:23 +0000

    1 com

---
+++ SCORE2/SCORE2/Class Patient.cs
@@ -0,0 +1,7 @@
+﻿class Patient
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public void PrintAll() => Console.WriteLine($"Patient: {FirstName}, {LastName}");
+}
---
+++ SCORE2/SCORE2/Program.cs
@@ -0,0 +1,7 @@
+﻿Patient Kanatev = new Patient();
+Kanatev.FirstName = "Konstantin";
+Kanatev.LastName = "Kanatev";
+Kanatev.Age = 39;
+Kanatev.PrintAll();
+
+
