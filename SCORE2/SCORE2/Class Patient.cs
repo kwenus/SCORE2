dacbe176841a7f0d9688e16c475736b246f73290
kwenus <kwenus82@gmail.com>
2022-02-09 23:44:25 +0300

    8 com

--- SCORE2/SCORE2/Class Patient.cs
+++ SCORE2/SCORE2/Class Patient.cs
@@ -4,6 +4,7 @@
     public string? LastName;
     public string? Sex;
     public int Age;
+    public string? Smoking;
     public Patient()
     {
         Console.WriteLine("Answer next questions.");
@@ -13,11 +14,16 @@
         LastName = Console.ReadLine();
         Console.WriteLine("What is your sex?");
         Sex = Console.ReadLine();
-        Console.WriteLine("What is your age");
+        Console.WriteLine("What is your age?");
         Age = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Are you smoking? Enter yes or no.");
+        Smoking = Console.ReadLine();
+
     }
     public void PrintAll()
     {
-        Console.WriteLine($"Patient: {FirstName}, {LastName}, {Sex}, {Age}");
+        Console.Write($"Patient: {FirstName}, {LastName}, {Sex}, {Age}.");
+        if (Smoking == "yes") Console.Write(" Smoking.");
+        else Console.Write(" No Smoking.");
     }
 }
